ebebbbe90ff601bd2b752f3f2296348fa3ce0a9a
tahaddad-hub <t.a.haddad@gmail.com>
2026-04-28 01:29:21 +0300

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -4,4 +4,5 @@ from supabase import create_client, Client
 def get_supabase_client() -> Client:
     url = st.secrets["keys"]["SUPABASE_URL"]
     key = st.secrets["keys"]["SUPABASE_PUBLISHABLE_KEY"]
+    st.write("Connecting to: " + url[:30] + "...")
     return create_client(url, key)
