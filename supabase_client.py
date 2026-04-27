cb4c0696d8e9b5aae74afeb042f2c531e90e57be
tahaddad-hub <t.a.haddad@gmail.com>
2026-04-28 02:07:09 +0300

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,8 +1,20 @@
 import streamlit as st
+import sys
+import os
 from supabase import create_client, Client
 
 def get_supabase_client() -> Client:
     url = st.secrets["keys"]["SUPABASE_URL"]
     key = st.secrets["keys"]["SUPABASE_PUBLISHABLE_KEY"]
-    st.write("Connecting to: " + url[:30] + "...")
-    return create_client(url, key)
+    
+    # Suppress connection message printed by supabase library
+    devnull = open(os.devnull, 'w')
+    old_stdout = sys.stdout
+    sys.stdout = devnull
+    
+    client = create_client(url, key)
+    
+    sys.stdout = old_stdout
+    devnull.close()
+    
+    return client
