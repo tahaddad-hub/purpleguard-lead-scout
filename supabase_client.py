fcec2c6459c00d276e872978827a0d5af79620bf
tahaddad-hub <t.a.haddad@gmail.com>
2026-04-28 00:41:52 +0300

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,1 +1,8 @@
+import streamlit as st
+from supabase import create_client, Client
 
+@st.cache_resource
+def get_supabase_client() -> Client:
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_PUBLISHABLE_KEY"]
+    return create_client(url, key)
