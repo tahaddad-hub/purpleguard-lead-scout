367c5fe7b0020c4aa126339ffbada0e1015051ee
tahaddad-hub <t.a.haddad@gmail.com>
2026-04-28 02:09:16 +0300

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -3,18 +3,19 @@ import sys
 import os
 from supabase import create_client, Client
 
+@st.cache_resource
 def get_supabase_client() -> Client:
     url = st.secrets["keys"]["SUPABASE_URL"]
     key = st.secrets["keys"]["SUPABASE_PUBLISHABLE_KEY"]
-    
+
     # Suppress connection message printed by supabase library
     devnull = open(os.devnull, 'w')
     old_stdout = sys.stdout
     sys.stdout = devnull
-    
+
     client = create_client(url, key)
-    
+
     sys.stdout = old_stdout
     devnull.close()
-    
+
     return client
