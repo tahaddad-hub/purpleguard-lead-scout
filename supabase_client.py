9c04cae6a5e240ecabbeca8da76da9505bbbfd01
tahaddad-hub <t.a.haddad@gmail.com>
2026-04-28 00:57:03 +0300

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -3,6 +3,6 @@ from supabase import create_client, Client
 
 @st.cache_resource
 def get_supabase_client() -> Client:
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_PUBLISHABLE_KEY"]
+    url = st.secrets["keys"]["SUPABASE_URL"]
+    key = st.secrets["keys"]["SUPABASE_PUBLISHABLE_KEY"]
     return create_client(url, key)
